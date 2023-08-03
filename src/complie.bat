5dd871ef2ab76f35e5e8afa494279498a89bd174
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2023-08-04 05:17:11 +0800

    Add files via upload

--- src/complie.bat
+++ src/complie.bat
@@ -1,1 +1,18 @@
 g++ -O3 -o core.exe colortest.cpp
+@echo off
+REM Get the parent folder (one level up)
+for %%I in ("%~dp0.") do set "PARENT_FOLDER=%%~dpI"
+
+REM Set the target folder where "scripts" exists
+set "TARGET_FOLDER=%PARENT_FOLDER%scripts"
+
+REM Check if the target folder exists, if not, create it
+if not exist "%TARGET_FOLDER%" mkdir "%TARGET_FOLDER%"
+
+REM Copy the file from the current folder to the target folder
+copy "%~dp0core.exe" "%TARGET_FOLDER%"
+
+REM Remove the original file after copying
+del "%~dp0core.exe"
+
+pause
