3d0d19100fbd17b5eb3e23f67fb2324325a4a101
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2023-08-04 04:38:30 +0800

    long solution format support

--- animate.bat
+++ animate.bat
@@ -2,5 +2,10 @@
 setlocal
 set "arg1=%~1"
 set "arg2=%~2"
-generate "%arg1%" "%arg2%" | core.exe
+
+REM Run the "slidy format" command and store the output in a temporary variable
+for /f "delims=" %%a in ('slidy format -l "%arg2%"') do set "temp_arg2=%%a"
+
+REM Run "generate" with the updated "arg2"
+generate "%arg1%" "%temp_arg2%" | core.exe
 endlocal
