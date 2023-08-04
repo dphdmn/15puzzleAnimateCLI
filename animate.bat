60c786cf5c0107f0665bc4a70af3a94d906084bb
Dmitry L <58388093+dphdmn@users.noreply.github.com>
2023-08-04 20:23:19 +0800

    Delete animate.bat

--- animate.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "arg1=%~1"
-set "arg2=%~2"
-set "scripts_folder=%~dp0scripts"
-for /f "delims=" %%a in ('slidy format -l "%arg2%"') do set "temp_arg2=%%a"
-%scripts_folder%\generate "%arg1%" "%temp_arg2%" | %scripts_folder%\core.exe
-endlocal
